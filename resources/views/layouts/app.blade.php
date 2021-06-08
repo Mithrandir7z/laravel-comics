514e6a4b17a43788842fac049018db966ac2c357
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-06-08 16:22:14 +0200

    sviluppo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Struttura base</title>
 
-    <link rel="stylesheet" href="{{  }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <!--QUESTO E' LO SCHELETRO DELL SITO-->
 </head>
 <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,66 @@
 <header>
-    <h1>header</h1>
+    <div class="row_top">
+
+        <div class="margine">
+            <ul>
+                <li>
+                    <a href="">DC POWER VISA</a>
+                </li>
+    
+                <li>
+                    <a href="">ADDITIONAL DC SITES</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <nav>
+        <div class="margine">
+            logo
+            <img src="" alt="">
+
+            <ul>
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
