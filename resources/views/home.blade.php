e5847fc28be08ca7e1b8bb3b7dca2fb1c22ac6f8
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-06-08 19:24:56 +0200

    aggiornamento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>Sono il main</h1>
+    <div class="main">
+        <ul>
+            
+        @foreach ($comics_array as $comic)
+            {{--Single Comic--}}
+            <li>
+                <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+            </li>
+        @endforeach
+            
+        </ul>
+    </div>
 @endsection
