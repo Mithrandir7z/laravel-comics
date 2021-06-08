6c5ad4919cfde93b18f59e3fe5a25b4d09c8ebdd
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-06-08 17:21:02 +0200

    fix partials scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,50 +17,61 @@
 
     <nav>
         <div class="margine">
-            logo
-            <img src="" alt="">
+            <div class="menu">
+                
+                <div class="logo">
+                    <img src="{{ asset('img/dc-logo.png') }}" alt="">
+                </div>
 
-            <ul>
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                <ul>
+                    <li>
+                        <a href="">CHARACTERS</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">COMICS</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">MOVIES</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">GAMES</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">COLLECTIBLES</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">VIDEOS</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">FANS</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
+                    <li>
+                        <a href="">NEWS</a>
+                    </li>
 
-                <li>
-                    <a href="">CHARACTERS</a>
-                </li>
-            </ul>
+                    <li>
+                        <a href="">SHOP</a>
+                    </li>
+
+                    <!--INPUT-->
+                    <li>
+                        <label for="">
+                            <input type="text">
+                        </label>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
