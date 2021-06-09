a4ac5512a62a5ebee0b6ace40d9237d4abe84ac3
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-06-09 17:30:55 +0200

    sviluppo ulteriore

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 
 @section('main_content')
+
+    <div class="blue_row">
+        <div class="image_product">
+            <img src="{{$product['thumb']}}" alt="{{$product['title']}}">
+        </div>
+    </div>
     
     <div class="product-info">
-        <div class="margine">
-            {{-- {{dd($product['title'])}} --}}
+        {{-- {{dd($product['title'])}} --}}
 
-            <div class="container">
+        <div class="general_container_product">
+            <div class="container_left">
                 <h1>{{$product['title']}}</h1>
-
+    
                 <div class="container_green_row">
                     <div class="green_row_left">
                         <div class="left">
@@ -24,12 +30,24 @@
                         </div>
                     </div>
                     <div class="green_row_right">
-                        
+                        <span>
+                            <p>Check Availability</p>
+                        </span>
                     </div>
                 </div>
+    
+                <p>
+                    {{$product['description']}}
+                </p>
+            </div> 
+    
+    
+            <div class="container_right">
+                <h4>ADVERTISEMENT</h4>
+                <img src="{{asset('img/a-d-v.jpg')}}" alt="{{$product['title']}}">
             </div>
-            
-        </div>  
+        </div>
+         
     </div>  
 
 @endsection
